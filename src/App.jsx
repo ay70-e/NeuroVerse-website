00a2d24ddbc3e30880b6547a2861f200a14bae56
ay70-e <ayalahebjaad@gmail.com>
2026-01-14 00:18:33 +0300

    version 0.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
+import Header from "./components/Header.jsx";
 import Brain from "./pages/Brain";
 import Consciousness from "./pages/Consciousness";
 import Dimensions from "./pages/Dimensions";
